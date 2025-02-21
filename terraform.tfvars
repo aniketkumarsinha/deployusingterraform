7e2638cab75773a1118bf1f2fa423e37d96488e4
aniket-sinha8 <sinhaaniket32@gmail.com>
2025-02-21 12:45:40 +0530

    tf infra with pipeline

---
+++ terraform.tfvars
@@ -0,0 +1,4 @@
+resource_group_name = "terraformrg"
+location = "centraindia"
+
+stg_name = "storageaccounttf"
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "resource_group_name" {
+  description = "The name of the resource group in which the resources will be created."
+}
+
+variable "location" {
+  description = "The location in which the resources will be created."
+}
+
+variable "stg_name" {
+  description = "The name of the storage account."  
+}
